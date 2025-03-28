3adacba0b9690f97bc2f539772e03d3903be4b66
Noah <azsnobird1227@yahoo.com>
2025-03-28 18:18:12 -0500

    results Categories baseline, links not added yet

--- src/components/results/resultsCategories.js
+++ src/components/results/resultsCategories.js
@@ -2,30 +2,30 @@ import { MdPlayArrow } from "react-icons/md";
 import Link from 'next/link';
 
 export default function ResultsCategories() {
-    <>
-      <div className="flex justify-start w-full flex-col font-semibold">
-        <div className="flex justify-between items-center w-full h-16">
-            <div className=""> 
-            SKINSTRIC <span className="grey"> &#91; ANALYSIS &#93; </span>
-            </div>
-        </div>
-        <div>
-            TO START ANALYSIS
-        </div>
-      </div>
-        <form >
-            <div className="dashBorder flex items-center flex-col pl-4 text-center justify-center">
-                <div className="grey textlabel text-sm unrotate pb-4 pr-16"> CLICK TO TYPE </div>
-                <input required id="location" className="textfield decoration-[1px] text-center underline underline-offset-8 text-5xl font-light unrotate" type="text" placeholder="Where are you from?" />
-            </div>
-        </form>
-      <div className="flex pb-4 w-full items-center">
-          <Link href="/introduce"> 
-          <div className="border"> 
-            <MdPlayArrow className="rotate-[135deg]" size={20}/>
-          </div>
-          </Link>
-            <div className="text-lg pl-4">  Back </div>
-        </div>
-    </>
+  return (
+    <div className="flex flex-col justify-center h-full">
+            <div className="fourdiamond text-center">
+                <div className="part top "> 
+                  <div className="unrotate">
+                    DEMOGRAPHICS
+                  </div>
+                </div>
+                <div className="part right"> 
+                  <div className="unrotate">
+                    COSMETIC CONCERNS
+                  </div>
+                </div>
+                <div className="part left"> 
+                  <div className="unrotate">
+                    SKIN TYPE DETAILS
+                  </div>
+                </div>
+                <div className="part bottom "> 
+                  <div className="unrotate">
+                    WEATHER
+                  </div>
+                </div>
+            </div> 
+    </div>
+  )
 }
--- src/pages/results.js
+++ src/pages/results.js
@@ -6,42 +6,74 @@ import { useState, useEffect } from 'react';
 import ResultsCategories from "@/components/results/resultsCategories";
 
 export default function Home() {
-    const axios = require('axios');
-    const router = useRouter();
+    // const axios = require('axios');
+    // const router = useRouter();
 
     const [status, setStatus] = useState("cat");
 
     const renderContent = (status) => {
       switch(status) {
-        // case "loading":
-        //   return <ResultsLoading />;
+        case "loading":
+          return < > </>;
         case "cat":
           return <ResultsCategories />;
-        // case "demos":
-        //   return <ResultsDemos />
+        case "demos":
+          return < > </>
+      }
+    }
+    const renderSubMessage = (status) => {
+      switch(status) {
+        case "loading":
+          return < > </>;
+        case "cat":
+          return < > 
+            <div className="font-normal"> A.I HAS ESTIMATED THE FOLLOWING.</div>
+            <div className="font-normal" > FIX ESTIMATED INFORMATION IF NEEDED. </div>
+          </>;
+        case "demos":
+          return < > </>
       }
     }
 
-    const { base64 } = router.query;
-    useEffect(() => {
+    // const { base64 } = router.query;
+    // useEffect(() => {
         
-        axios({
-            method: 'post',
-            url: 'https://us-central1-frontend-simplified.cloudfunctions.net/skinstricPhaseTwo',
-            data: {
-                image: base64
-            }
-          })
-          .then(function(response) {
-            console.log(response)
+    //     axios({
+    //         method: 'post',
+    //         url: 'https://us-central1-frontend-simplified.cloudfunctions.net/skinstricPhaseTwo',
+    //         data: {
+    //             image: base64
+    //         }
+    //       })
+    //       .then(function(response) {
+    //         console.log(response)
             
-          })
-    }, [base64, axios])
+    //       })
+    // }, [base64, axios])
 
 
   return (
-    <main className="w-full h-full">
-       <ResultsCategories />
+    <main className="w-full h-full flex flex-col items-center ">
+      <div className="pl-4 flex justify-start w-full flex-col font-semibold">
+        <div className="flex justify-between items-center w-full h-16">
+            <div className=""> 
+            SKINSTRIC <span className="grey"> &#91; ANALYSIS &#93; </span>
+            </div>
+        </div>
+        <div className="pb-2">
+            A.I ANALYSIS
+        </div>
+        {renderSubMessage(status)}
+      </div>
+       {renderContent(status)}
+       <div className="pl-4 flex pb-4 w-full items-center">
+          <Link href="/introduce"> 
+          <div className="border"> 
+            <MdPlayArrow className="rotate-[135deg]" size={20}/>
+          </div>
+          </Link>
+            <div className="text-lg pl-4">  Back </div>
+        </div>
     </main> 
   );
 }
