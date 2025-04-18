b425e0ce4ea989367760c87297269bab1c27b35f
Noah <azsnobird1227@yahoo.com>
2025-04-18 11:33:47 -0500

    responsiveness fixes on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,22 +12,22 @@ export default function Home() {
           ENTER CODE
         </div>
       </div>
-      <div className="flex flex-row justify-between w-full items-center">
-        <div className="flex items-center">
+      <div className="flex flex-col sm:flex-row justify-between w-full items-center">
+        <div className="flex items-center pb-24 pr-24 sm:p-0">
         <div className="dashBorder-left"> </div>
           <Link href="/"> 
             <div className="border"> 
               <MdPlayArrow className="arrowIcon rotate-[135deg]" />
             </div>
           </Link>
-            <div className="text-sm pl-3 md:pl-6">  DISCOVER A.I. </div>
+            <div className="text-xs sm:text-sm pl-3 md:pl-6">  DISCOVER A.I. </div>
         </div>
         <div className=" w-[680px] mr-2 text-9xl font-light flex items-center justify-center text-center title">
           Sophisticated skincare
         </div>
-        <div className="flex items-center">
+        <div className="flex pt-24 pl-24 sm:p-0 items-center">
           <div className="dashBorder-right"> </div>
-          <div className="text-sm pl-2 pr-2 sm:pr-3 md:pr-6"> TAKE TEST </div>
+          <div className="text-xs sm:text-sm pl-2 pr-2 sm:pr-3 md:pr-6"> TAKE TEST </div>
           <Link href="/introduce"> 
             <div className="border"> 
               <MdPlayArrow className="-rotate-45 arrowIcon" />
