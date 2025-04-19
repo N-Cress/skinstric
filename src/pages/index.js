d9ffb7949338b9999e8cac9289a8a975a6b99634
Noah <azsnobird1227@yahoo.com>
2025-04-18 21:41:45 -0500

    hover effect on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import { MdPlayArrow } from "react-icons/md";
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Home() {
+  const [hovered, setHovered] = useState(false);
+
   return (
     <main className="flex flex-col items-center justify-between h-full w-full pl-8 pr-8">
       <div className="flex justify-between items-center w-full h-16">
@@ -13,24 +16,26 @@ export default function Home() {
         </div>
       </div>
       <div className="flex flex-col sm:flex-row justify-between w-full items-center">
-        <div className="flex items-center pb-24 pr-24 sm:p-0">
+        <div className={`flex items-center pb-24 pr-24 sm:p-0 transition-all duration-500 ease-in ${hovered ? `hidden`: ``}`}>
         <div className="dashBorder-left"> </div>
           <Link href="/"> 
             <div className="border"> 
               <MdPlayArrow className="arrowIcon rotate-[135deg]" />
             </div>
           </Link>
-            <div className="text-xs sm:text-sm pl-3 md:pl-6">  DISCOVER A.I. </div>
+            <div className={`text-xs sm:text-sm pl-3 md:pl-6`}>  DISCOVER A.I. </div>
         </div>
-        <div className=" w-[680px] mr-2 text-9xl font-light flex items-center justify-center text-center title">
+        <div className={`w-[680px] mr-2 text-9xl font-light flex items-center justify-center text-center title ${hovered ? `pl-24` : ``}`}>
           Sophisticated skincare
         </div>
         <div className="flex pt-24 pl-24 sm:p-0 items-center">
           <div className="dashBorder-right"> </div>
           <div className="text-xs sm:text-sm pl-2 pr-2 sm:pr-3 md:pr-6"> TAKE TEST </div>
           <Link href="/introduce"> 
-            <div className="border"> 
-              <MdPlayArrow className="-rotate-45 arrowIcon" />
+            <div onMouseEnter={() => setHovered(true)} onMouseLeave={()=> setHovered(false)} className="border"> 
+              <div className={`outerBorder-arrow  ${hovered ? `border-2 border-gray-200 border-dashed ` : ``}`}>
+                <MdPlayArrow  className={`-rotate-12 arrowIcon transition-all duration-500 ease-in ${hovered ? `p-1` : ``}`} />
+              </div>
             </div>
           </Link>
         </div>
